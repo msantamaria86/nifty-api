2b29d10187df6a2adebff159418d7133e9ce4161
msantamaria86 <msantamaria86@gmail.com>
2023-03-30 10:16:53 -0500

    abi array3

--- api.js
+++ api.js
@@ -14,7 +14,7 @@ app.use(morgan('dev'));
 const provider = new ethers.providers.JsonRpcProvider("https://rpc.slock.it/goerli"); 
 const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider); 
 const contractAddress = process.env.CONTRACT_ADDRESS; 
-const contract = new ethers.Contract(contractAddress, abi, signer);
+const contract = new ethers.Contract(contractAddress, abi.abi, signer);
 
 app.get('/api', (req, res)  => {
   res.json({test: test1})
