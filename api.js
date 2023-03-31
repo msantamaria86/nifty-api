058b5d8e167da77fe0cb0030e8b53f5194e0646e
msantamaria86 <msantamaria86@gmail.com>
2023-03-30 21:41:40 -0500

    provider1

--- api.js
+++ api.js
@@ -16,7 +16,9 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(morgan('dev'));
 
-const provider = new EtherscanProvider("goerli");
+const network = ethers.providers.getNetwork("goerli");
+const provider = new EtherscanProvider(network);
+
 // const provider = new ethers.providers.JsonRpcProvider("https://rpc.slock.it/goerli"); 
 const signer = new ethers.Wallet(process.env.PRIVATE_KEY, provider); 
 const contractAddress = process.env.CONTRACT_ADDRESS; 
