2206f317832e74f07ef287a1bf16b8fb6a810b53
msantamaria86 <msantamaria86@gmail.com>
2023-03-30 20:57:38 -0500

    new fix express2

--- api.js
+++ api.js
@@ -1,15 +1,13 @@
 require('dotenv').config()
 const abi = require('./Marketplace.json');
-const bp = require('body-parser')
+const bodyParser = require('body-parser')
 const {ethers, utils} = require('ethers');
 const express = require('express');
 const cors = require('cors');
 const morgan  = require('morgan');
 
 const app = express()
-app.use(bp.json())
-app.use(bp.urlencoded({ extended: true }))
-
+const jsonParser = bodyParser.json()
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
@@ -24,8 +22,9 @@ app.get('/api', (req, res)  => {
   res.json({test: "test1"})
   })
 
-app.post('/create', async function(req, res) { 
+app.post('/create', jsonParser, async function(req, res) { 
   
+    console.log('BODY', req.body)
   try {
     const nftContract = req.body.contract;
     const nftId = req.body.id;
