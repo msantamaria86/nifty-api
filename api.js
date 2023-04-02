232995d83e02299b4ebbfb8caf8387a198d9ef4d
msantamaria86 <msantamaria86@gmail.com>
2023-04-02 17:22:09 -0500

    price test2

--- api.js
+++ api.js
@@ -24,8 +24,8 @@ const contract = new ethers.Contract(contractAddress, abi.abi, signer);
 
 app.get('/api', async function (req, res) {
   const id = await contract.getPrice(listingId)
-  });
   console.log('Transaction id:', id)
+  });
 
 app.post('/create', jsonParser, async function(req, res) { 
   
